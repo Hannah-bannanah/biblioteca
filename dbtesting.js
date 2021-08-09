589634f3278be30ca17c84bc816390fdbe2840ee
hannah-bannanah <Hannah-bannanah@users.noreply.github.com>
2021-08-09 13:27:41 +0200

    eliminado carpeta web, añadido rutas y funcionalidad para ver catalogo, registrar libro, actualizar libro, ver pagina de 404

--- dbtesting.js
+++ dbtesting.js
@@ -12,24 +12,11 @@ const Libro = require('./models/libro.model');
 // Libro.getLibro('9780099470465').then(result => console.log(result));
 
 const libro = new Libro(
-    isbn = 'prueba4', titulo = 'prueba', id_editorial = 1, genero = 'prueba', idioma = 'prueba', numero_paginas = 10, anyo_edicion = 2020, ejemplares = 1
+    isbn = 'prueba', titulo = 'prueba', id_editorial = 1, genero = 'prueba', idioma = 'prueba', numero_paginas = 10, ejemplares = 1
 );
 
-// Libro.getLibro('9788497932950').then(result => {
-//     console.log(result[0]);
-//     return result;
-// }).then(result => {
-//     if (result[0].length > 0) return true;
-//     else return false;
-// }).then(result => {
-//     if (result) console.log("book exists");
-//     else console.log("book doesn't exist");
-// });
+const libro2 = new Libro(
+    isbn = 'prueba', titulo = 'PRUEBA2', id_editorial = 2, genero = 'GENERO2', idioma = 'CLINGON2', numero_paginas = 1022, ejemplares = 2
+);
 
-// Libro.bookExists('9788497932950').then(result => {
-//     if (result) console.log("book exists");
-//     else console.log("book doesn't exist");
-// });
-
-// Libro.getLibro('9788497932950').then(result => console.log(result));
-Libro.bookExists('9788497932950').then(result => console.log(result));
+Libro.addBook(libro).then(result => console.log(result));
