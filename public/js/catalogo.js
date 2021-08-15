0ff962db11749ed8dbb41d1d146b7b35dd5ee5ff
hannah-bannanah <Hannah-bannanah@users.noreply.github.com>
2021-08-15 13:01:07 +0200

    udpated catalog carrousel so it opens at specific book

--- public/js/catalogo.js
+++ public/js/catalogo.js
@@ -1,27 +1,26 @@
-var idxLibro = 0;
+let fichaLibroIdx;
 
-function abrirCarrousel() {
-    idxLibro = 0;
-    document.getElementById('carrousel-libros').style.display="block";
+function abrirCarrousel(idxLibro) {
+    fichaLibroIdx = idxLibro;
+    document.getElementById('carrousel-libros').style.display = "block";
     mostrarLibros();
 }
-
-function cerrarParent(elementId) {
-    document.getElementById(elementId).parentNode.style.display="none";
-}
-
 
 function siguienteLibro(direccion) {
-    idxLibro += direccion;
-    mostrarLibros();
+    fichaLibroIdx += direccion;
+    mostrarLibros(fichaLibroIdx);
 }
 
-function mostrarLibros(){
+function mostrarLibros() {
     var libros = document.getElementsByClassName("carrousel");
-    for (i=0; i<libros.length; i++){
-        libros[i].style.display="none";
+    for (i = 0; i < libros.length; i++) {
+        libros[i].style.display = "none";
     }
-    if (idxLibro >= libros.length){idxLibro = 0;}
-    if (idxLibro < 0) {idxLibro = libros.length - 1;}
-    libros[idxLibro].style.display = "block";
+    if (fichaLibroIdx >= libros.length) {
+        fichaLibroIdx = 0;
+    }
+    if (fichaLibroIdx < 0) {
+        fichaLibroIdx = libros.length - 1;
+    }
+    libros[fichaLibroIdx].style.display = "block";
 }
